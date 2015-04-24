a7d72d77da1759d2d13b5c0ebd79267ac1dfdd7a
Brayden Ware <braydenware@gmail.com>
2015-04-23 17:39:13 -0700

    added a correlation bound plot

--- images/CorrelationBounds.py
+++ images/CorrelationBounds.py
@@ -39,3 +39,25 @@ for L in Ls:
 from tabulate import tabulate
 data = [(L,)+scb_ans[L]+hcb_ans[L] for L in Ls]
 print tabulate(data, ['L', 'SCB O', '', 'SCB S', '', 'HCB O', '', 'HCB S', ''])
+
+from SimplePEPS.tools.plot import subplots
+(fig, axs), bigax = subplots(2, 2, sharex='all', sharey='row')
+
+plt.sca(axs[0][0])
+plt.plot([d[0] for d in data], [d[1] for d in data], ls='', marker='.')
+plt.sca(axs[1][0])
+plt.plot([d[0] for d in data], [d[3] for d in data], ls='', marker='.')
+plt.sca(axs[0][1])
+plt.plot([d[0] for d in data], [d[5] for d in data], ls='', marker='.')
+plt.sca(axs[1][1])
+plt.plot([d[0] for d in data], [d[7] for d in data], ls='', marker='.')
+
+
+bigax.set_ylabel('Correlation Length', labelpad=12)
+bigax.set_xlabel('W')
+axs[0][0].set_ylabel('Overall')
+axs[0][0].set_title('Soft-Core')
+axs[0][1].set_title('Hard-Core')
+axs[1][0].set_ylabel('Symmetric')
+plt.tight_layout()
+plt.show()
